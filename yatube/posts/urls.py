5dc2efed1ba171d888784f7e34007a9c84520925
Тэмур Махмудов <verloren454@gmail.com>
2022-05-05 02:13:21 +0300

    Разделение urlpatterns

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('group/', views.group_posts),
-    path('group/<slug:list_name>/', views.group_list),
+    path('group/<slug:slug>/', views.group_list),
 ]

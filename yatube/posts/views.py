5f628280ac93209cd77fb368d4385e0e5d4e9414
Тэмур Махмудов <verloren454@gmail.com>
2022-05-05 05:12:01 +0300

    Render

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Главная страница')
+    return render(request, 'posts/index.html')
 
 def group_posts(request):
     return HttpResponse('Группы постов')
